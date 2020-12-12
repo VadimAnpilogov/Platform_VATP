a7e24f55f22283036f8523de5e670f39ff1d8666
VadimAnpilogov <vadick.anpilogov2015@yandex.ru>
2020-12-12 18:22:30 +0700

    Добавление прав доступа к ролям

---
+++ src/main/java/com/platform_vatp/model/Permission.java
@@ -0,0 +1,16 @@
+package com.platform_vatp.model;
+
+public enum Permission {
+    DEVELOPERS_READ("developer:read"),
+    DEVELOPERS_WRITE("developers:write");
+
+    private final String permission;
+
+    Permission(String permission) {
+        this.permission = permission;
+    }
+
+    public String getPermission() {
+        return permission;
+    }
+}
