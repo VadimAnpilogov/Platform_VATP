fa3439dd14664ad4adc295a0762e61398c90606a
VadimAnpilogov <vadick.anpilogov2015@yandex.ru>
2020-12-12 15:51:25 +0700

    Основа проекта

---
+++ src/test/java/com/platform_vatp/PlatformVatpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platform_vatp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlatformVatpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
